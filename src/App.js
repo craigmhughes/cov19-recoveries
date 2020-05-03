58d19b84bd3e2d1febf0c59ab08d00b001632738
Craig Hughes <hghscraig@gmail.com>
2020-05-03 09:22:22 +0100

    Returning correct data from API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const axios = require('axios').default;
+
+  const [data, setData] = useState(null);
+
+  /**
+   * Perform GET request to API endpoint for UK data on recovered cases.
+   *
+   */
+  function getData(){
+    axios.get('https://api.covid19api.com/total/country/united-kingdom/status/recovered').then(res=>{
+
+      // Get latest entry.
+      let latest = res.data[res.data.length-1];
+
+      // Overwrite with useless keys omitted && Overwrite date with Date object.
+      latest = {
+        Cases: latest.Cases,
+        Date: new Date(latest.Date)
+      };
+
+      console.log(latest);
+    }).catch(err=>console.error(err));
+  }
+
+  useEffect(()=>{
+    getData();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
