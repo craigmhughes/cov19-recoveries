d850cdee9acd7b6a8c1641d99805ba5ecf5fea8c
Craig Hughes <hghscraig@gmail.com>
2020-05-04 01:25:47 +0100

    Updates

---
+++ precache-manifest.8b65ed8e3946b02d0d64dcb809705b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a416edcb2c65e3a6b9e4134f463a9c0",
+    "url": "/cov19-recoveries/index.html"
+  },
+  {
+    "revision": "47f2ce4a5fef0368cbb8",
+    "url": "/cov19-recoveries/static/css/main.00910e0f.chunk.css"
+  },
+  {
+    "revision": "99f8f2321678219726bb",
+    "url": "/cov19-recoveries/static/js/2.5141f5b4.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/cov19-recoveries/static/js/2.5141f5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f2ce4a5fef0368cbb8",
+    "url": "/cov19-recoveries/static/js/main.cb417e6a.chunk.js"
+  },
+  {
+    "revision": "42ee8f71c82f56422d2d",
+    "url": "/cov19-recoveries/static/js/runtime-main.d3148e66.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,86 @@
+/* eslint-disable no-restricted-globals */
+
+let cacheName = "v1";
+
+// Must cache all necessary files in order to work offline.
+let filesToCache = [
+    "/",
+    "/manifest.json",
+    "/static/css/app.css",
+    "/static/js/app.js",
+    // Fonts
+    "/fonts/questrial-regular.woff2",
+    // Models
+    "/heart.obj",
+    // Icons
+    "/heart-192.png",
+    "/heart-192-ico.png",
+];
+
+// On installing the service worker.
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(cacheName)
+        .then((cache)=>{
+            cache.addAll(filesToCache)
+        })
+        .then(()=> self.skipWaiting())
+        .catch(err=> console.log(err))
+    );
+});
+
+// On the service worker's activation.
+self.addEventListener("activate", e => {
+    e.waitUntil(
+
+        // Remove unwanted Caches
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName){
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+// Keywords to cache
+const assetsToCache = ["js", "css"];
+
+
+// Listen for fetch requests.
+self.addEventListener("fetch", e => {
+    // Loop over Keywords to cache assets which match.
+    for(let asset of assetsToCache){
+
+        if(e.request.url.includes(asset)){
+            return e.respondWith(
+                caches.match(e.request).then(res=>{
+                    return caches.open(cacheName).then((cache)=>{
+                        // Carry out fetch request and cache response.
+                        return fetch(e.request).then(resp => {
+                            cache.put(e.request, resp.clone());
+                            return resp;
+                            
+                        }).catch(err=>{
+                            console.log(err);
+                            return false;
+                        });
+                    }) || res;
+                })
+                
+            );
+        }
+    }
+    
+    e.respondWith(
+        // If requested fetch is cached, return cached version.
+        caches.match(e.request).then( res =>{
+            // Uncomment and comment out the line below to block connections.
+            // return res || false;
+            return fetch(e.request).catch(err=>{return res});
+        })
+    ); 
+});
